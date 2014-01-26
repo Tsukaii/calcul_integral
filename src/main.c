4eb088b7e99c2b2c2c4564831588f8f6231b2d29
Julie Grastilleur <jgrastilleur@gmail.com>
2014-01-26 16:11:10 +0100

    Ajout de la vérification des valeurs interdites dans le main

--- src/main.c
+++ src/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "simpson.h"
 #include "romberg.h"
-#include "trapeze.h"
 
 //Options du menu des fonctions
 #define POLYNOME 1
@@ -15,6 +14,9 @@
 
 #define NBCHOIX 7
 
+//Précision utilisée pour la méthode de Romberg
+#define PREC 15
+
 int main(int argc, char *argv[]){
 	int fonction = 0;
 	int saisieCorrecte = 0;
@@ -26,16 +28,16 @@ int main(int argc, char *argv[]){
 	double integrale_s = 0.0; //Intégrale calculée avec la méthode Simpson
 	double integrale_r = 0.0; //Intégrale calculée avec la méthode Romberg
 	
+	double temp = 0.0; //Variable temporaire utilisé pour la vérification des bornes
 	double borneInf = 0.0; //Borne inférieure de l'intervalle
 	double borneSup = 0.0; //Borne inférieure de l'intervalle
-	int nbPts = 0.0; //Nombre de sous-intervalles
+	int nbPts = 0; //Nombre de sous-intervalles
 	
 	printf("\n--------------------------------------------------------\n");
 	printf("-      Calcul de l'approximation d'une intégrale       -\n");
 	printf("--------------------------------------------------------\n\n");
 	
 	printf("Appuyer sur le numéro correspondant à votre choix\n");
-	
 	
 	//Choix de la fonction à utiliser
 	while ( (fonction > NBCHOIX || fonction < 1) && (!saisieCorrecte) ) {
@@ -69,6 +71,13 @@ int main(int argc, char *argv[]){
 	scanf("%lf", &borneInf);
 	printf("Borne supérieure : ");
 	scanf("%lf", &borneSup);
+	
+	if (borneSup < borneInf){
+		temp = borneSup;
+		borneSup = borneInf;
+		borneInf = temp;
+	}
+	
 	printf("Nombre de sous-intervalles (valeur max conseillée = 1 000 000) : ");
 	scanf("%d", &nbPts);
 	
@@ -91,33 +100,41 @@ int main(int argc, char *argv[]){
 			}
 			
 			integrale_s = methodeSimpsonPolynome(borneInf, borneSup, nbPts, polynome, degre);
-			integrale_r = methodeRombergPolynome(borneInf, borneSup, 15, 15, polynome, degre);
+			integrale_r = methodeRombergPolynome(borneInf, borneSup, PREC, PREC, polynome, degre);
 			
 			break;
 			
 		case SINUS :
 			integrale_s = methodeSimpsonSinus(borneInf, borneSup, nbPts);
-			integrale_r = methodeRombergSinus(borneInf, borneSup, 15, 15);
+			integrale_r = methodeRombergSinus(borneInf, borneSup, PREC, PREC);
 			break;
 			
 		case RACINELN :
-			integrale_s = methodeSimpsonRacineLn(borneInf, borneSup, nbPts);
-			integrale_r = methodeRombergRacineLn(borneInf, borneSup, 15, 15);
+			if (borneInf <= 0 || borneSup <= 0)
+				printf("Votre intervalle contient une valeur interdite !\n");
+			else{
+				integrale_s = methodeSimpsonRacineLn(borneInf, borneSup, nbPts);
+				integrale_r = methodeRombergRacineLn(borneInf, borneSup, PREC, PREC);
+			}
 			break;
 			
 		case SINUSCUBE :
 			integrale_s = methodeSimpsonSinusCube(borneInf, borneSup, nbPts);
-			integrale_r = methodeRombergSinusCube(borneInf, borneSup, 15, 15);
+			integrale_r = methodeRombergSinusCube(borneInf, borneSup, PREC, PREC);
 			break;
 		
 		case INVTAN4 :
-			integrale_s = methodeSimpsonInverseTanExpQuatre(borneInf, borneSup, nbPts);
-			integrale_r = methodeRombergInverseTanExpQuatre(borneInf, borneSup, 15, 15);
+			if (borneInf <= 0 && borneSup >= 0)
+				printf("Votre intervalle contient une valeur interdite !\n");
+			else{
+				integrale_s = methodeSimpsonInverseTanExpQuatre(borneInf, borneSup, nbPts);
+				integrale_r = methodeRombergInverseTanExpQuatre(borneInf, borneSup, PREC, PREC);
+			}
 			break;
 			
 		case COS2EXP :
 			integrale_s = methodeSimpsonCosCarreExp(borneInf, borneSup, nbPts);
-			integrale_r = methodeRombergCosCarreExp(borneInf, borneSup, 15, 15);
+			integrale_r = methodeRombergCosCarreExp(borneInf, borneSup, PREC, PREC);
 			break;
 			
 		default :
@@ -127,8 +144,8 @@ int main(int argc, char *argv[]){
 	if (fonction == POLYNOME)
 		free(polynome); //Libération de l'espace mémoire du polynôme saisi par l'utilisateur
 		
-	printf("L'intégrale de votre fonction, calculée avec la méthode Simpson, vaut %g\n", integrale_s);
+	printf("\nL'intégrale de votre fonction, calculée avec la méthode Simpson, vaut %g\n", integrale_s);
 	printf("L'intégrale de votre fonction, calculée avec la méthode Romberg, vaut %g\n", integrale_r);
-	
+
 	return 0;
 }
